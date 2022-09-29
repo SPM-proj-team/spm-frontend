8ea14238d43b39537134f08df5a61bd0a7fbabac
absolutelynoot <isalsamudra@hotmail.com>
2022-09-30 01:38:16 +0800

    Initialized view roles

---
+++ src/components/JobRolesCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <div class="row row-cols-md-3 g-2 g-lg-3 justify-content-center">
+            <div v-for="jobRole in jobRoles" :key="jobRole.id" class="col ">
+                <button class="btn btn-light card w-100 h-100">
+                    <div class="card-body w-100">
+                        <h5 class="card-title ">{{jobRole.role}}</h5>
+                        <p class="card-text"> {{jobRole.position}}</p>
+                    </div>
+                </button>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+
+export default {
+    props: ['jobRoles']
+    
+}
+
+</script>
+
+<style scoped>
+
+</style>
--- src/views/JobRoles.vue
+++ src/views/JobRoles.vue
@@ -1,10 +1,31 @@
 <template>
-  <div class="container mt-3 text-center">
-    <h1>Roles</h1>
+  <div class="container">
+    <h1 class="my-5 text-center text-gray-500">Job Roles</h1>
+    <JobRolesCard :jobRoles="jobRoles"/>
   </div>
 </template>
 
-<script setup>
+<script>
+
+import JobRolesCard from '../components/JobRolesCard.vue'
+
+export default {
+  components: {
+    JobRolesCard
+  },
+  data () {
+    return {
+      jobRoles: [
+        {id:1, role: 'Analytics Executive', position: 'Executive'},
+        {id:2, role: 'Business Development Manager', position: 'Manager'},
+        {id:1, role: 'Analytics Executive', position: 'Executive'},
+        {id:4, role: 'Business Development Manager', position: 'Manager'},
+        {id:1, role: 'Analytics Executive', position: 'Executive'},
+        {id:6, role: 'Business Development Manager', position: 'Manager'}
+      ]
+    }
+  }
+}
 
 </script>
 
