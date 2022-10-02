f0dabac41ddfeda44bf6bbb4b71217a6e188a36a
absolutelynoot <isalsamudra@hotmail.com>
2022-10-03 02:18:32 +0800

    Added pre-work for user LJ

--- src/components/MyLearningJourneys.vue
+++ src/components/MyLearningJourneys.vue
@@ -1,45 +1,25 @@
 <template>
-  <button
-    class="btn btn-light card w-100 h-100 my-3 p-0 text-start"
-    v-for="learningJourney in learningJourneys"
-    :key="learningJourney.Learning_Journey_ID"
-  >
-    
-      <div class="row g-0">
-        <div class="col-md-8">
-          <div class="col">
-            <div class="card-body">
-              <h5 class="card-title">
-                {{ learningJourney.Learning_Journey_Name }}
-              </h5>
-              <p class="card-text">{{ learningJourney.Description }}</p>
-              <p class="card-text">
-                <small class="text-muted"
-                  >{{Object.keys(learningJourney.Courses).length}} Courses</small
-                >
-              </p>
+    <div class="card my-3" >
+        <div class="row g-0">
+            <div class="col-md-4">
+                <!-- <img src="..." class="img-fluid rounded-start" alt="..."> -->
             </div>
-          </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
         </div>
-      </div>
-    
-    </button>
+    </div>
 </template>
 
 <script>
-import { userStore } from "@/store";
 
-export default {
-  setup() {
-    const store = userStore();
-    return { store };
-  },
-
-  name: "MyLearningJourneys",
-  props: {
-    learningJourneys: Object,
-  },
-};
 </script>
 
-<style scoped></style>
+<style scoped>
+
+</style>
