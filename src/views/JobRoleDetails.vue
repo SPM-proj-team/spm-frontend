c8854c0216594db71d870359f64a4674f1d80ede
absolutelynoot <isalsamudra@hotmail.com>
2022-10-04 21:58:33 +0800

    Created Skills Requirements component and updated job role details view

--- src/views/JobRoleDetails.vue
+++ src/views/JobRoleDetails.vue
@@ -1,18 +1,24 @@
 <template>
     <div class="container">
         <h1 class="my-5 text-center text-gray-500">Job Roles Details</h1>
-        <p class="text-center">The job id is {{ JobRoleID }}</p>
         <div class="container my-3">
             <SelectedJobRole :SelectedJobRole="jobRoleDetails"></SelectedJobRole>
-        </div>
-        <div class="conatiner my-3">
-            <SkillsCard :Skills="jobRoleDetails.Skills" />
+            <div class="row justify-content-center g-2">
+                <div class="col-4">
+                    <SkillsRequirements :Skills="jobRoleDetails.Skills" />
+                </div>
+                <div class="col">
+                    <SkillsCard :Skills="jobRoleDetails.Skills" />
+                </div>
+            </div>
+
         </div>
     </div>
 </template>
 
 <script>
 import SelectedJobRole from '@/components/SelectedJobRole.vue';
+import SkillsRequirements from '@/components/SkillRequirements.vue'
 import SkillsCard from '@/components/SkillsCard.vue';
 import { userStore } from '@/store';
 import axios from 'axios'
@@ -34,11 +40,12 @@ export default {
     methods: {
         // Get individual job details based on route params (JobRole_ID)
         getJobDetails() {
-            const path = 'http://127.0.0.1:5000/roles';
+            const path = 'http://127.0.0.1:5000/roles/' + this.JobRoleID;
+            console.log(path)
             axios.get(path)
                 .then((res) => {
                     // console.log(res.data.data);
-                    this.jobRoleDetails = res.data.data[this.JobRoleID - 1];
+                    this.jobRoleDetails = res.data.data[0];
                     console.log(this.jobRoleDetails)
                 })
                 .catch((err) => {
@@ -58,7 +65,8 @@ export default {
 
     components: {
         SelectedJobRole,
-        SkillsCard
+        SkillsCard,
+        SkillsRequirements
     }
 }
 
