22502234524e0619684b68af4050e34f1b5a4118
Eric Ho <73128965+HoJiYuanEric@users.noreply.github.com>
2022-10-07 17:04:16 +0800

    Revert "Merge remote-tracking branch 'origin/View-Skills-(Roles-Details)' into Sprint-1"
    This reverts commit 6e9d106c1c3962e760ca57ceda27b23c41e68a7a, reversing
    changes made to b831c3d2274bdbfed942787c8c5cf0045e3fbe83.

--- src/components/SelectedJobRole.vue
+++ src/components/SelectedJobRole.vue
@@ -1,15 +1,15 @@
 <template>
-    
+    <div>
         <div class="card">
-            <div class="card-header fw-bold text-center text-bg-dark">
+            <div class="card-header fw-bold text-center">
                 Selected Job Role
             </div>
             <div class="card-body">
-                <h5 class="card-title text-center fw-bold">{{ SelectedJobRole.Job_Role }}</h5>
+                <h5 class="card-title text-center">{{ SelectedJobRole.Job_Role }}</h5>
                 <p class="card-text  text-center">{{ SelectedJobRole.Job_Title }}</p>
             </div>
         </div>
-    
+    </div>
 </template>
 
 <script>
--- src/views/JobRoleDetails.vue
+++ src/views/JobRoleDetails.vue
@@ -1,10 +1,11 @@
 <template>
     <div class="container">
-        <h1 class="my-5 text-center text-gray-500 fw-bold">Job Roles Details</h1>
+        <h1 class="my-5 text-center text-gray-500">Job Roles Details</h1>
+        <p class="text-center">The job id is {{ JobRoleID }}</p>
         <div class="container my-3">
             <SelectedJobRole :SelectedJobRole="jobRoleDetails"></SelectedJobRole>
         </div>
-        <div class="conatiner my-3">
+        <div class="container my-3">
             <SkillsCard :Skills="jobRoleDetails.Skills" />
         </div>
     </div>
@@ -12,7 +13,6 @@
 
 <script>
 import SelectedJobRole from '@/components/SelectedJobRole.vue';
-import SkillsRequirements from '@/components/SkillFulfillment.vue'
 import SkillsCard from '@/components/SkillsCard.vue';
 import { userStore } from '@/store';
 import axios from 'axios'
@@ -25,10 +25,7 @@ export default {
     },
     data() {
         return {
-            jobRoleDetails: [],
-            jobRoleSkills: [],
-            selectedCourses: [],
-            mappedCourses: [],
+            jobRoleDetails: []
         }
     },
     mounted() {
@@ -37,72 +34,18 @@ export default {
     methods: {
         // Get individual job details based on route params (JobRole_ID)
         getJobDetails() {
-            const path = 'http://127.0.0.1:5000/roles/' + this.JobRoleID;
-            console.log("Retrieving role detail from " + path)
+            const path = 'http://127.0.0.1:5000/roles';
             axios.get(path)
                 .then((res) => {
-
-                    // get full details
-                    this.jobRoleDetails = res.data.data[0];
-                    console.log("Job Role Details: ")
+                    // console.log(res.data.data);
+                    this.jobRoleDetails = res.data.data[this.JobRoleID - 1];
                     console.log(this.jobRoleDetails)
-
-                    // process
-                    this.getSkills()
                 })
                 .catch((err) => {
                     console.log(err);
                     // this.$router.push({ name: 'NotFound404' });
                 })
-        },
-
-        // process selected courses and map it accordingly
-        mapCourses(selectedCourses) {
-
-            // Log selected courses
-            this.selectedCourses = selectedCourses;
-            console.log("User has selected the following courses;");
-            console.log(this.selectedCourses);
-
-
-            // Map selected courses to all relevant skills
-            var temp_mappedCourses = [];
-            console.log("Mapping selected courses to all relevant skills...");
-
-            for (var Skill of this.jobRoleDetails.Skills) {
-                // temp_mappedCourses[Skill.Skill_ID] = []
-                for (var Course of Skill.Courses) {
-                    if (this.selectedCourses.includes(Course.Course_ID)) {
-                        temp_mappedCourses.push({
-                            'Skill_ID': Skill.Skill_ID,
-                            'Course_ID': Course.Course_ID,
-                            'Course_Name': Course.Course_Name,
-                        })
-                    }
-                }
-            }
-
-            this.mappedCourses = temp_mappedCourses;
-            console.log("Course(s) has been mapped to relevant skill(s). Details:")
-            console.log(temp_mappedCourses)
-
-        },
-
-        // process skill from all details to reduce UI load
-        getSkills() {
-            var skills = []
-            for (let skill of this.jobRoleDetails.Skills) {
-                skills.push(skill);
-            }
-
-            // log
-            console.log("All Skills:");
-            console.log(skills)
-
-            this.jobRoleSkills = skills
         }
-
-
     },
     props: ['JobRoleID'],
 
@@ -115,8 +58,7 @@ export default {
 
     components: {
         SelectedJobRole,
-        SkillsCard,
-        SkillsRequirements
+        SkillsCard
     }
 }
 
