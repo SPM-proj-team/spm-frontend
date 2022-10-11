9a950f8c2be7ad0027e81c28a80e5c8f546a7304
absolutelynoot <isalsamudra@hotmail.com>
2022-10-11 12:32:14 +0800

    Update JobRolesSekelton.vue

--- src/components/JobRolesSekelton.vue
+++ src/components/JobRolesSekelton.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-2 g-lg-3 justify-content-center align-content-center">
-            <div v-for="index in 9" :key="index" class="col">
+            <div v-for="index in 12" :key="index" class="col">
                     <div class="card w-100 h-100 shadow-sm">
                         <div class="card-body w-100 text-start ">
                             <h5 class="card-title my-auto fw-semibold placeholder-glow"><span class="placeholder w-75"></span></h5>
