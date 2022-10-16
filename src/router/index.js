9a8df588fd7314fb6c7cdb949909c1d18fc0874b
absolutelynoot <isalsamudra@hotmail.com>
2022-10-16 14:47:41 +0800

    published update-learning-journey

--- src/router/index.js
+++ src/router/index.js
@@ -22,6 +22,12 @@ const routes = [
     props: true
   },
   {
+    path: '/LearningJourney/:LJID',
+    name: 'JobRoleDetails',
+    component: JobRoleDetails,
+    props: true
+  },
+  {
     path: '/CreateLearningJourney',
     name: 'Create Learning Journey',
     component: CreateLearningJourney
