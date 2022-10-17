51e7747820e3e5e6c369cc19a6561de0e9fa660d
absolutelynoot <isalsamudra@hotmail.com>
2022-10-17 13:19:09 +0800

    Update LearningJourneyInfo.vue

--- src/components/LearningJourneyInfo.vue
+++ src/components/LearningJourneyInfo.vue
@@ -1,20 +1,22 @@
 <template>
     <div class="card px-3 shadow">
         <div class="card-body">
-            <h3 class="card-title fw-semibold my-3">Learning Journey Details</h3>
-            <div class="form-floating mb-3">
-                <input type="search" class="form-control" id="lj-name" placeholder="Learning Journey Name"
-                    v-model="this.name">
-                <label for="lj-name">Name</label>
-            </div>
-            <div class="form-floating mb-3">
-                <textarea type="search" 
-                class="form-control" 
-                id="lj-description" placeholder="Learning Journey Description" 
-                style="height: 150px; max-height: 200px; min-height: 100px;"
-                    v-model="this.description"></textarea>
-                <label for="lj-description">Description</label>
-            </div>
+            <form>
+                <h3 class="card-title fw-semibold my-3">Learning Journey Details</h3>
+                <div class="form-floating mb-3">
+                    <input type="text" readonly class="form-control-plaintext" id="lj-name"
+                        placeholder="Learning Journey Name" v-model="this.name" @focusin="clickChangeInputType"
+                        @focusout="clickChangeInputTypeReadOnly">
+                    <label for="lj-name">Name</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <textarea type="search" class="form-control-plaintext" readonly id="lj-description"
+                        placeholder="Learning Journey Description"
+                        style="height: 150px; max-height: 200px; min-height: 200px;" v-model="this.description"
+                        @focusin="clickChangeInputType" @focusout="clickChangeInputTypeReadOnly"></textarea>
+                    <label for="lj-description">Description</label>
+                </div>
+            </form>
         </div>
     </div>
 </template>
@@ -26,10 +28,24 @@ export default {
         ljName: String,
         ljDescription: String
     },
-    data(){
-        return{
+    data() {
+        return {
             name: this.ljName,
             description: this.ljDescription
+        }
+    },
+    methods: {
+        clickChangeInputType(event) {
+            if (event.target.classList.contains("form-control-plaintext")) {
+                event.target.classList = ["form-control"]
+                event.target.removeAttribute('readonly')
+            }
+        },
+        clickChangeInputTypeReadOnly(event) {
+            if (event.target.classList.contains("form-control")) {
+                event.target.classList = ["form-control-plaintext"]
+                event.target.addAttribute('readonly')
+            }
         }
     }
 }
